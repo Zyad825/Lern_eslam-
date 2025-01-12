f431f7ac96212cde5663a4f358682a94e6507782
Zyad825 <zyadgammer111@gmail.com>
2025-01-12 21:29:34 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+
+        // دالة لفتح وإغلاق القائمة في الأجهزة الصغيرة
+        function toggleMenu() {
+            const navLinks = document.querySelector('.nav-links');
+            navLinks.classList.toggle('active');
+        }
+//new line
+
+
